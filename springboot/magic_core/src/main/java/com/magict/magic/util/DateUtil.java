5c18c49fb94be5889388165573a9d0aed235179a
zp501881208 <501881208@qq.com>
2018-05-10 14:11:28 +0800

    时间工具类，http工具类

--- springboot/magic_core/src/main/java/com/magict/magic/util/DateUtil.java
+++ springboot/magic_core/src/main/java/com/magict/magic/util/DateUtil.java
@@ -791,4 +791,21 @@ public class DateUtil {
         calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH)+day);
         return calendar.getTime();
     }
+
+    /**
+     * 时间计算，加hour小时min分钟second秒后的日期，可用负数表示减，不加减传0
+     * @param date
+     * @param hour
+     * @param min
+     * @param second
+     * @return
+     */
+    public static Date calTime(Date date,int hour,int min,int second){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY,calendar.get(Calendar.HOUR_OF_DAY)+hour);
+        calendar.set(Calendar.MINUTE,calendar.get(Calendar.MINUTE)+min);
+        calendar.set(Calendar.SECOND,calendar.get(Calendar.SECOND)+second);
+        return calendar.getTime();
+    }
 }
--- springboot/magic_core/src/main/java/com/magict/magic/util/RequestUtil.java
+++ springboot/magic_core/src/main/java/com/magict/magic/util/RequestUtil.java
@@ -2,6 +2,7 @@ package com.magict.magic.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -34,5 +35,36 @@ public class RequestUtil {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * 获取真实ip
+     * @param request
+     * @return
+     */
+    public static String getIpAddress(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if(StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)){
+            ip = request.getHeader("X-Real-IP");
+        }
+        if (StringUtils.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if(ip!=null){
+            return ip.split(",")[0];
+        }
+        return ip;
     }
 }
