bd7db5601321c7c59e9ef0f7e3ee8eef05665d94
zp501881208 <501881208@qq.com>
2018-05-10 19:50:40 +0800

    增加管理端部分

--- springboot/magic_mcp/src/main/java/com/magict/magic/MagicMcpApplication.java
+++ springboot/magic_mcp/src/main/java/com/magict/magic/MagicMcpApplication.java
@@ -1,16 +1,14 @@
 package com.magict.magic;
 
-
 import com.magict.magic.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.magict.magic.mapper")
-public class MagicStareApplication{
+public class MagicMcpApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(MagicStareApplication.class, args);
-
-
+		SpringApplication.run(MagicMcpApplication.class, args);
 	}
 }
