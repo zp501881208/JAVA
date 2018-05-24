ff9b0d67acc8eb12c6fcfb4656ca3feb1677ed65
zp501881208 <501881208@qq.com>
2018-05-24 16:19:21 +0800

    菜单样式

--- springboot/magic_mcp/README.txt
+++ springboot/magic_mcp/README.txt
@@ -1,2 +1,4 @@
 1.项目名称在配置文件中配置server.context-path=mcp
 2.context-path配置导致前端a标签链接容易丢失项目名称路径，必须统一使用@{}的方式，如：<a th:href="@{${menu.functionUrl}}">
+3.菜单icon图标在[font-awesome.min.css]中
+4.菜单的选中样式在myPage.css中，js/myPage.js中控制菜单选中和自动匹配子菜单
--- springboot/magic_mcp/src/main/resources/static/assets/js/common.js
+++ springboot/magic_mcp/src/main/resources/static/assets/js/common.js
@@ -1,24 +1,59 @@
-(function(){
-    var tDiv = document.getElementsByClassName("nav"),
-        index = 0,//默认第一个菜单项
-        urla = location.href.split('?')[0].split('/');
-    urla.pop();
-    var url = urla.pop();
-    var links = new Array();
-    links = $(".nav-list li a");
-    if(url){
-        for (var i=links.length; i--;) {
-            var a=links[i].href.split('?')[0].split('/');
-            a.pop();
-            a = a.pop();
-            if(a==url){
-                index = i;
-                break;
+// (function(){
+//     // var tDiv = document.getElementsByClassName("nav"),
+//     index = 0;//默认第一个菜单项
+//     // urla = location.href.split('?')[0].split('/');
+//     var url = document.location.pathname;
+//     // var url = urla[4]+"/"+urla[5]
+//     // urla.pop();
+//     // var url = urla.pop();
+//     var links = new Array();
+//     // links = $(".nav-list li a");
+//     links = $(".submenu");
+//
+//     if(url){
+//         for (var i=links.length; i--;) {
+//             var b = links[i].attr("href").charAt('?');
+//             var c = links[i].attr("href");
+//             if(b>0){
+//                 var a=links[i].href.split('?')[0].split('/');
+//             }else{
+//                 var a=links[i].href.split('/');
+//             }
+//             console.log(a);
+//             a.pop();
+//             a = a.pop();
+//             if(a==url){
+//                 index = i;
+//                 break;
+//             }
+//         }
+//     }
+//     links.eq(index).closest("li").addClass("active").siblings().removeClass("active");
+// })();
+
+$(document).ready(function(){
+    //默认选中第一个
+    var cc = $(".nav-list").children(":first").addClass("active");
+    //获取访问路径跟菜单functionUrl匹配
+    var pathurl = document.location.pathname;
+    // var links1 = $(".submenu");
+    var links2 = $(".submenu").children("li").children("a");
+    if(links2){
+        for (var i=links2.length;i--;){
+            var a = links2[i];
+            var url = a.href;
+            if(url.indexOf(pathurl)>0){
+                var c = a.parentNode;
+                $(c).addClass("active").siblings().removeClass("active");
+                var d = c.parentNode.parentNode;
+                $(d).addClass("active").siblings().removeClass("active");
             }
         }
     }
-    links.eq(index).closest("li").addClass("active").siblings().removeClass("active")
-})();
+
+});
+
+
 //confirm框
 function confirmModal(conMsg,conHref){
     var conTitle = "确认", // 提示 标题
@@ -52,4 +87,17 @@ function showMyModal(conTitle,conMsg,conHref,conClose,conSubmit){
         $(".consubmit").hide();
     }
     $('.mymodal').modal();
+}
+
+//cookie存储用户最近点击菜单，用于当前菜单高亮显示，展开二级菜单
+// var currentMenuId=null;//当前菜单
+// $(function () {
+//     var menuId = $.cookie('the_cookie');
+//     if(menuId!=undefined){
+//         currentMenuId = menuId;
+//     }
+// });
+
+function setCurrentMenu(e) {
+    $(e).addClass("active").siblings().removeClass("active");
 }
